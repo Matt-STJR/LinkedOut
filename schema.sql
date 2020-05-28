eec66aca2aefb43a795aca774415545725c2b6d3
Caitlin Mulcahy <caitlinmulcahy1@gmail.com>
2020-05-28 21:48:37 +1000

    first commit

--- schema.sql
+++
@@ -1,8 +0,0 @@
--- FOR TESTING PURPOSES--
-
--- Drops the linkedout_db if it exists currently --
-DROP DATABASE IF EXISTS linkedout_db;
--- Creates the "linkedout_db" database --
-CREATE DATABASE linkedout_db;
-
-  
