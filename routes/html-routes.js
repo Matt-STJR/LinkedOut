3c7046c6968c7c3bdefcc6751915340878edb0c4
Caitlin Mulcahy <caitlinmulcahy1@gmail.com>
2020-05-30 11:24:27 +1000

    updating

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,10 +6,10 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
 
-  app.get("/", function(req, res) {
+  app.get("/LinkedOut", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/LinkedOut/home");
     }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
@@ -17,15 +17,15 @@ module.exports = function(app) {
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/LinkedOut/home");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+  app.get("/LinkedOut/home", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
 };
